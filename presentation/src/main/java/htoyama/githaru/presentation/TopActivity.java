c3fe93f125f09dfbb7ff3171036add19115f45d4
egugue <toyamaosamuyu@toyamaosamuyu-no-MacBook-Air.local>
2015-05-02 23:42:06 +0900

    rename presentation package

--- presentation/src/main/java/htoyama/githaru/presentation/TopActivity.java
+++ presentation/src/main/java/htoyama/githaru/presentation/TopActivity.java
@@ -1,9 +1,11 @@
-package htoyama.githaru;
+package htoyama.githaru.presentation;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import htoyama.githaru.R;
 
 
 public class TopActivity extends ActionBarActivity {
@@ -14,26 +16,18 @@ public class TopActivity extends ActionBarActivity {
         setContentView(R.layout.activity_top);
     }
 
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_top, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
             return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 }
