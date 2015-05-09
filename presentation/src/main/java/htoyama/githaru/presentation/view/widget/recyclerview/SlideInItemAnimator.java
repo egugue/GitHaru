9871a9c2c1edf02c09268e7196f5bb84fcb011cc
egugue <toyamaosamuyu@toyamaosamuyu-no-MacBook-Air.local>
2015-05-09 18:48:10 +0900

    initial animator

---
+++ presentation/src/main/java/htoyama/githaru/presentation/view/widget/recyclerview/SlideInItemAnimator.java
@@ -0,0 +1,8 @@
+package htoyama.githaru.presentation.view.widget.recyclerview;
+
+/**
+ * Created by toyamaosamuyu on 2015/05/09.
+ */
+public class SlideInItemAnimator {
+    private static final String TAG = SlideInItemAnimator.class.getSimpleName();
+}
